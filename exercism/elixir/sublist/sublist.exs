3c5c0df6dd5fed0bf6972f418d29344b88bb8f7c
Eugene Pirogov <iamexile@gmail.com>
2016-06-13 20:03:40 +0300

    [exorcism.io] [elixir] sublist (more cleanup)

--- exercism/elixir/sublist/sublist.exs
+++ exercism/elixir/sublist/sublist.exs
@@ -17,19 +17,14 @@ defmodule Sublist do
     end
   end
 
+  # comparing lists of equal size
   def _compare(_, _, false), do: false
   def _compare([], [], true), do: true
-  def _compare([h1|t1], [h2|t2], _) do
-    _compare(t1, t2, h1 == h2)
-  end
+  def _compare([h1|t1], [h2|t2], _), do: _compare(t1, t2, h1 == h2)
 
-  def _compare([], super, _, _) when length(super) >= 0, do: true
-  def _compare(sub, super, _, _) when length(sub) > length(super), do: false
-  def _compare([h1|t1], [h2|t2], backup1, [_|bt] = backup2) do
-    if h1 === h2 do
-      _compare(t1, t2, backup1, backup2)
-    else
-      _compare(backup1, bt, backup1, bt)
-    end
-  end
+  # comparing small list (l1) against big list (l2)
+  def _compare([], l2, _, _) when length(l2) >= 0, do: true
+  def _compare(l1, l2, _, _) when length(l1) > length(l2), do: false
+  def _compare([h1|t1], [h2|t2], l1bckp, l2bckp) when h1 === h2, do: _compare(t1, t2, l1bckp, l2bckp)
+  def _compare([h1|_], [h2|_], l1bckp, [_|t]) when h1 !== h2, do: _compare(l1bckp, t, l1bckp, t)
 end
