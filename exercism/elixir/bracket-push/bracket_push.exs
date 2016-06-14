6db807e2d6ae832c90ed5d8aadce7c7b599d5cbe
Eugene Pirogov <iamexile@gmail.com>
2016-06-14 20:13:34 +0300

    [exorcism.io] [elixir] bracket-push

---
+++ exercism/elixir/bracket-push/bracket_push.exs
@@ -0,0 +1,29 @@
+defmodule BracketPush do
+  @doc """
+  Checks that all the brackets and braces in the string are matched correctly, and nested correctly
+  """
+  @spec check_brackets(String.t) :: boolean
+  def check_brackets(str) do
+    check(List.flatten(Regex.scan(~r/\p{Ps}|\p{Pe}/, str)), [])
+  end
+
+  defp check([], []), do: true
+  defp check([], _), do: false
+  defp check([h1|t1], []), do: check(t1, [h1])
+  defp check([h1|t1], [h2|t2] = stack) do
+    cond do
+      h1 =~ ~r/\p{Ps}/ ->
+        check(t1, [h1 | stack])
+      h1 =~ ~r/\p{Pe}/ && friends?(h2, h1) ->
+        check(t1, t2)
+      true ->
+        false
+    end
+  end
+
+  defp friends?(s, e) do
+    s == "(" && e == ")" ||
+    s == "[" && e == "]" ||
+    s == "{" && e == "}"
+  end
+end
