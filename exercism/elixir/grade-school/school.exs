91f0ee0d46f01aef305f62144576ee3b9595c6ae
Eugene Pirogov <iamexile@gmail.com>
2016-06-17 12:58:14 +0300

    [exorcism.io] [elixir] school

---
+++ exercism/elixir/grade-school/school.exs
@@ -0,0 +1,38 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """
+
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(map, String.t, integer) :: map
+  def add(db, name, grade) do
+    {_, output} = Map.get_and_update(db, grade, fn
+      nil -> {nil, [name]}
+      names -> {names, [name | names]}
+    end)
+
+    output
+  end
+
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(map, integer) :: [String.t]
+  def grade(db, grade) do
+    Map.get(db, grade, [])
+  end
+
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(map) :: [{integer, [String.t]}]
+  def sort(db) do
+    db
+    |> Enum.sort
+    |> Enum.map(fn {k, v} -> {k, Enum.sort(v)} end)
+  end
+end
