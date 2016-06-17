230bfc195e0b4b7d4ba0fb6cc9732068d5db4564
Eugene Pirogov <iamexile@gmail.com>
2016-06-17 18:34:49 +0300

    [exorcism.io] [elixir] etl

---
+++ exercism/elixir/etl/etl.exs
@@ -0,0 +1,16 @@
+defmodule ETL do
+  @doc """
+  Transform an index into an inverted index.
+
+  ## Examples
+
+  iex> ETL.transform(%{"a" => ["ABILITY", "AARDVARK"], "b" => ["BALLAST", "BEAUTY"]})
+  %{"ability" => "a", "aardvark" => "a", "ballast" => "b", "beauty" =>"b"}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    Enum.reduce(input, %{}, fn {score, words}, result -> 
+      Map.merge(result, for(word <- words, into: %{}, do: { String.downcase(word), score }))
+    end)
+  end
+end
