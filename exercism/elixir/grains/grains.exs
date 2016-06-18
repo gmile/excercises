5d588bf537a505fbe7e0d65a3d2317ec23ff171c
Eugene Pirogov <iamexile@gmail.com>
2016-06-18 15:31:03 +0300

    [exorcism.io] [elixir] grains (fun cleanup)
    Just for fun.
    
    I think the fastest solution would be using bit operators, though.

--- exercism/elixir/grains/grains.exs
+++ exercism/elixir/grains/grains.exs
@@ -3,20 +3,14 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer) :: pos_integer
-  def square(number) do
-    [acc, _] = do_square(number, 1, 0)
-    acc
-  end
-
-  defp do_square(1, acc, total), do: [acc, 1 + total]
-  defp do_square(n, acc, total), do: do_square(n - 1, acc * 2, total + acc * 2)
+  def square(number), do: do_square(number, 1, 0)[:value]
 
   @doc """
   Adds square of each number from 1 to 64.
   """
   @spec total :: pos_integer
-  def total do
-    [_, total] = do_square(64, 1, 0)
-    total
-  end
+  def total, do: do_square(64, 1, 0)[:total]
+
+  defp do_square(1, acc, total), do: %{value: acc, total: 1 + total}
+  defp do_square(n, acc, total), do: do_square(n - 1, acc * 2, total + acc * 2)
 end
