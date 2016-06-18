ab67fb4e16d497461c6e91ba71fae9084cbb4d09
Eugene Pirogov <iamexile@gmail.com>
2016-06-18 15:39:59 +0300

    [exorcism.io] [elixir] grains (cleanup)

--- exercism/elixir/grains/grains.exs
+++ exercism/elixir/grains/grains.exs
@@ -1,14 +1,6 @@
 defmodule Grains do
   import Bitwise, only: ["<<<": 2]
-  @doc """
-  Calculate two to the power of the input minus one.
-  """
-  @spec square(pos_integer) :: pos_integer
-  def square(number), do: 1 <<< (number - 1)
 
-  @doc """
-  Adds square of each number from 1 to 64.
-  """
-  @spec total :: pos_integer
+  def square(number), do: 1 <<< (number - 1)
   def total, do: (1 <<< 64) - 1
 end
