71235a60be30dfefbe4c1825d90bdac584d022da
Eugene Pirogov <iamexile@gmail.com>
2016-06-18 14:36:04 +0300

    [exorcism.io] [elixir] space_age (cleanup)
    Still getting used to declaring module-level data, e.g. constants like
    this.

--- exercism/elixir/space-age/space_age.exs
+++ exercism/elixir/space-age/space_age.exs
@@ -1,18 +1,21 @@
 defmodule SpaceAge do
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :neptune | :uranus
+  @period %{
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
 
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
-  def age_on(:earth, seconds), do: seconds / 31557600
-  def age_on(:mercury, seconds), do: age_on(:earth, seconds) / 0.2408467
-  def age_on(:venus, seconds), do: age_on(:earth, seconds) / 0.61519726
-  def age_on(:mars, seconds), do: age_on(:earth, seconds) / 1.8808158 
-  def age_on(:jupiter, seconds), do: age_on(:earth, seconds) / 11.862615 
-  def age_on(:saturn, seconds), do: age_on(:earth, seconds) / 29.447498 
-  def age_on(:uranus, seconds), do: age_on(:earth, seconds) / 84.016846 
-  def age_on(:neptune, seconds), do: age_on(:earth, seconds) / 164.79132 
+  def age_on(planet, seconds), do: seconds / 31557600 / @period[planet]
 end
