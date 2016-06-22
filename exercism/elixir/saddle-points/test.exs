0593c0f1b27dd490bea883e156e79b1227c0095e
Eugene Pirogov <iamexile@gmail.com>
2016-06-22 11:39:48 +0300

    A step forward 2

--- exercism/elixir/saddle-points/test.exs
+++ exercism/elixir/saddle-points/test.exs
@@ -1,4 +1,20 @@
-a = [[3,4,5], [1,2,7],[4,5,6]]
+# a = [
+#   [18, 3, 39, 19, 91],
+#   [38, 10, 8, 77, 320],
+#   [3, 4, 8, 6, 7]
+# ]
+
+a = [
+  [4, 5, 4],
+  [3, 5, 5],
+  [1, 5, 4]
+]
+
+# a = [
+#   [1, 2],
+#   [3, 4]
+# ]
+
 b =
   a
   |> Enum.with_index
@@ -17,18 +33,20 @@ IO.inspect(b)
 
 row = 0
 
-c   = Enum.filter(b, fn {{_, r}, _} -> r == row end) # get row by index
-{{_, _,}, max} = Enum.max_by(c, fn {_, v} -> v end ) # find max in a row
+for row <- 0..length(a)-1 do
+  c   = Enum.filter(b, fn {{_, r}, _} -> r == row end) # get row by index
+  {_, max} = Enum.max_by(c, fn {_, v} -> v end) # find max in a row
 
-c
-|> Enum.filter(fn {_, v} -> v == max end)      # find all maxes in that row
-|> Enum.filter(fn {{max_x, max_y}, max_val} -> # iterate over all maxes
-     b
-     |> Enum.filter(fn {{x, y}, _} -> {x, y} != {max_x, max_y} && x == max_x end) # find all points in that column (except current max)
-     |> Enum.all?(fn {_, v2} -> max_val <= v2 end)                                # check if they are all "good"
-   end)
-|> Enum.map(fn {c, _} -> c end)
-|> IO.inspect
+  c
+  |> Enum.filter(fn {_, v} -> v == max end)      # find all maxes in that row
+  |> Enum.filter(fn {{max_x, max_y}, max_v} -> # iterate over all maxes
+       b
+       |> Enum.filter(fn {{x, y}, _} -> {x, y} != {max_x, max_y} && x == max_x end) # find all points in that column (except current max)
+       |> Enum.all?(fn {_, v} -> max_v <= v end)                                # check if they are all "good"
+     end)
+  |> Enum.map(fn {coordinates, _} -> coordinates end)
+  |> IO.inspect
+end
 
 # IO.puts("B: ")
 # IO.inspect(b)
