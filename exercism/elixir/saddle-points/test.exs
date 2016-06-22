4ed1d0339a0e99c31ba5ba748f04ab6b14284a51
Eugene Pirogov <iamexile@gmail.com>
2016-06-22 11:26:51 +0300

    A step forward

--- exercism/elixir/saddle-points/test.exs
+++ exercism/elixir/saddle-points/test.exs
@@ -17,25 +17,21 @@ IO.inspect(b)
 
 row = 0
 
-# Filter row
-c   = Enum.filter(b, fn {{_,r},_} -> r == row end)
-{{_, _,}, max} = Enum.max_by(c, fn {_,v} -> v end )
-all_max = Enum.filter(c, fn {_,v} -> v == max end)
+c   = Enum.filter(b, fn {{_, r}, _} -> r == row end) # get row by index
+{{_, _,}, max} = Enum.max_by(c, fn {_, v} -> v end ) # find max in a row
 
-IO.inspect(all_max)
-point = { {max_x, max_y} = {2, 0}, max_val = 5}
+c
+|> Enum.filter(fn {_, v} -> v == max end)      # find all maxes in that row
+|> Enum.filter(fn {{max_x, max_y}, max_val} -> # iterate over all maxes
+     b
+     |> Enum.filter(fn {{x, y}, _} -> {x, y} != {max_x, max_y} && x == max_x end) # find all points in that column (except current max)
+     |> Enum.all?(fn {_, v2} -> max_val <= v2 end)                                # check if they are all "good"
+   end)
+|> Enum.map(fn {c, _} -> c end)
+|> IO.inspect
 
-# saddle_points_0 = Enum.filter(all_max, fn {coords1, v1} ->
-
-  b = Enum.filter(b, fn {{x, y}, _} -> { x, y } != {max_x, max_y} && x == max_x end)
-  c = Enum.all?(b, fn {_, v2} -> max_val <= v2 end)
-# end)
-
-IO.puts("B: ")
-IO.inspect(b)
-IO.puts("C: ")
-IO.inspect(c)
-
-IO.inspect(max)
-IO.inspect(all_max)
+# IO.puts("B: ")
+# IO.inspect(b)
+#
+# IO.inspect(max)
 # IO.inspect(saddle_points_0)
