f301b4322f0457422f56dbb3a2468a839751aa5d
Eugene Pirogov <iamexile@gmail.com>
2016-06-22 16:09:38 +0300

    [exorcism.io] [elixir] saddle_point (cleanup)
    This is beautiful.

--- exercism/elixir/saddle-points/saddle_points.exs
+++ exercism/elixir/saddle-points/saddle_points.exs
@@ -25,34 +25,9 @@ defmodule Matrix do
   """
   @spec saddle_points(String.t()) :: [{integer, integer}]
   def saddle_points(str) do
-    a = rows(str)
-
-    b =
-      a
-      |> Enum.with_index
-      |> Enum.map(fn {line, i} ->
-        line
-        |> Enum.with_index
-        |> Enum.map(&Tuple.append(&1, i))
-      end)
-      |> List.flatten
-      |> Enum.reduce(%{}, fn {value, i, j}, acc ->
-        put_in(acc, [{j, i}], value)
-      end)
-
-    for row <- 0..length(a)-1 do
-      c = Enum.filter(b, fn {{r, _}, _} -> r == row end) # get row by index
-      {_, max} = Enum.max_by(c, fn {_, v} -> v end)        # find max in a row
-
-      c
-      |> Enum.filter(fn {_, v} -> v == max end)    # find all maxes in that row
-      |> Enum.filter(fn {{max_y, max_x}, max_v} -> # iterate over all maxes
-           b
-           |> Enum.filter(fn {{y, x}, _} -> {y, x} != {max_y, max_x} && x == max_x end) # find all points in that column (except current max)
-           |> Enum.all?(fn {_, v} -> max_v <= v end)                                # check if they are all "good"
-         end)
-      |> Enum.reduce([], fn {point, _}, acc -> [point | acc] end)
-    end
-    |> List.flatten
+    for {row, j} <- rows(str) |> Enum.with_index,
+        {col, i} <- columns(str) |> Enum.with_index,
+        Enum.min(col) == Enum.max(row),
+        do: {j, i}
   end
 end
