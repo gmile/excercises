4370c0f2c21d5d367e65c9b4b3f1971af204b47b
Eugene Pirogov <iamexile@gmail.com>
2016-06-25 14:26:09 +0300

    [exorcism.io] [elixir] crypto-square

---
+++ exercism/elixir/crypto-square/crypto_square.exs
@@ -0,0 +1,43 @@
+defmodule CryptoSquare do
+  @doc """
+  Encode string square methods
+  ## Examples
+
+    iex> CryptoSquare.encode("abcd")
+    "ac bd"
+  """
+  @spec encode(String.t) :: String.t
+  def encode(""), do: ""
+  def encode(str) do
+    str =
+      str
+      |> String.downcase
+      |> String.replace(~r|[^\w]|, "")
+      |> String.to_char_list
+
+    cols = cols_rows(length(str))
+
+    padding =
+    String.duplicate(" ", cols)
+    |> String.to_char_list
+
+    str
+    |> Enum.chunk(cols, cols, padding)
+    |> List.zip
+    |> Enum.map_join(" ", fn(e) ->
+      e
+      |> Tuple.to_list
+      |> List.to_string
+      |> String.trim_trailing
+    end)
+  end
+
+  def cols_rows(length) do
+    (for r <- 1..length,
+        c <- 1..length,
+        c >= r,
+        c <= r + 1,
+        length <= c * r,
+     do: c) |> hd
+  end
+end
