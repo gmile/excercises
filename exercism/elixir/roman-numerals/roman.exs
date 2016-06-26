a2356de8e026cda176fb4cb20daa1b06fef07539
Eugene Pirogov <iamexile@gmail.com>
2016-06-26 10:45:54 +0300

    [exorcism.io] [elixir] roman (cleanup)
    This is inspired by the following solutions:
    
      http://exercism.io/submissions/8f9e6b81aeef4792acac582933b2c846
      http://exercism.io/submissions/f7f8be940bca4898977ac64a3ab6d92a

--- exercism/elixir/roman-numerals/roman.exs
+++ exercism/elixir/roman-numerals/roman.exs
@@ -1,38 +1,30 @@
 defmodule Roman do
+  @mapping [
+    { 1000, "M" },
+    { 900,  "CM" },
+    { 500,  "D" },
+    { 400,  "CD" },
+    { 100,  "C" },
+    { 90,   "XC" },
+    { 50,   "L" },
+    { 40,   "XL" },
+    { 10,   "X" },
+    { 9,    "IX" },
+    { 5,    "V" },
+    { 4,    "IV" },
+    { 1,    "I" }
+  ]
+
   @doc """
   Convert the number to a roman number.
   """
-  @spec numerals(pos_integer) :: String.t
-  def numerals(number) do
-    number
-    |> convert
-    |> Enum.reverse
-    |> Enum.join
-  end
+  @spec numerals(pos_integer, String.t) :: String.t
+  def numerals(number, acc \\ "")
+  def numerals(0, acc), do: acc
 
-  def convert(number, d \\ 1000, acc \\ [])
-  def convert(number, 0, acc), do: [part(number, 1) | acc]
-  def convert(number, d, acc), do: convert(rem(number, d), div(d, 10), [part(number, d) | acc])
-
-  def part(number, d) do
-    { a, b, c } = digits(d)
-
-    case div(number, d) do
-      0 -> ""
-      1 -> a
-      2 -> a <> a
-      3 -> a <> a <> a
-      4 -> a <> b
-      5 -> b
-      6 -> b <> a
-      7 -> b <> a <> a
-      8 -> b <> a <> a <> a
-      9 -> a <> c
+  for { arabic, roman } <- @mapping do
+    def numerals(number, acc) when number >= unquote(arabic) do
+      numerals(number - unquote(arabic), acc <> unquote(roman))
     end
   end
-
-  def digits(1), do: { "I", "V", "X" }
-  def digits(10), do: { "X", "L", "C" }
-  def digits(100), do: { "C", "D", "M" }
-  def digits(1000), do: { "M", nil, nil }
 end
