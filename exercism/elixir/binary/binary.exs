90d587f1c06228d6b0fdf8ed40414ceed989f709
Eugene Pirogov <iamexile@gmail.com>
2016-06-26 11:05:56 +0300

    [exorcism.io] [elixir] binary (initial)

---
+++ exercism/elixir/binary/binary.exs
@@ -0,0 +1,13 @@
+defmodule Binary do
+  import Bitwise, only: [<<<: 2]
+  @doc """
+  Convert a string containing a binary number to an integer.
+
+  On errors returns 0.
+  """
+  @spec to_decimal(String.t) :: non_neg_integer
+  def to_decimal(string), do: convert(String.to_char_list(string), 0)
+  defp convert([], acc), do: acc
+  defp convert([?0 | t], acc), do: convert(t, acc)
+  defp convert([?1 | t], acc), do: convert(t, acc + 1 <<< length(t))
+end
