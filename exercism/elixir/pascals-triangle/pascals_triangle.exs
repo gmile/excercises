fbea7410f8b384ac3bf12e31c2edf84dbee24f4e
Eugene Pirogov <iamexile@gmail.com>
2016-07-06 12:44:25 +0300

    [exorcism.io] [elixir] pascal_triangle (cleanup)

--- exercism/elixir/pascals-triangle/pascals_triangle.exs
+++ exercism/elixir/pascals-triangle/pascals_triangle.exs
@@ -8,12 +8,8 @@ defmodule PascalsTriangle do
     do_rows(1, num, [[1]])
   end
 
-  def do_rows(i, num, [h | _] = acc) do
-    cond do
-      i < num -> do_rows(i + 1, num, [row(h, []) | acc])
-      i == num -> Enum.reverse(acc)
-    end
-  end
+  def do_rows(i, n, [h | _] = acc) when i < n, do: do_rows(i + 1, n, [row(h, []) | acc])
+  def do_rows(_, _, acc), do: Enum.reverse(acc)
 
   def row([1], acc), do: [1 | Enum.reverse([1 | acc])]
   def row([a, b | tail], acc), do: row([b | tail], [a + b | acc])
