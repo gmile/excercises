4efd6df43050120b301927dcbd8c65cad35ef81f
Eugene Pirogov <iamexile@gmail.com>
2016-07-07 13:13:17 +0300

    [exorcism.io] [elixir] hexademical (improve)

--- exercism/elixir/hexadecimal/hexadecimal.exs
+++ exercism/elixir/hexadecimal/hexadecimal.exs
@@ -1,4 +1,5 @@
 defmodule Hexadecimal do
+  import Bitwise, only: [<<<: 2]
   @doc """
     Accept a string representing a hexadecimal value and returns the
     corresponding decimal value.
@@ -17,10 +18,40 @@ defmodule Hexadecimal do
 
   @spec to_decimal(binary) :: integer
   def to_decimal(hex) do
-    try do
-      String.to_integer(hex, 16)
-    rescue
-      ArgumentError -> 0
+    if hex =~ ~r/^[A-Fa-f0-9]+$/ do
+      hex
+      |> String.downcase
+      |> String.to_char_list
+      |> Enum.reverse
+      |> Enum.with_index
+      |> Enum.reduce(0, fn({ char, index }, acc) ->
+        n = case char do
+          ?a -> 10
+          ?b -> 11
+          ?c -> 12
+          ?d -> 13
+          ?e -> 14
+          ?f -> 15
+          ?0 -> 0
+          ?1 -> 1
+          ?2 -> 2
+          ?3 -> 3
+          ?4 -> 4
+          ?5 -> 5
+          ?6 -> 6
+          ?7 -> 7
+          ?8 -> 8
+          ?9 -> 9
+        end
+
+        if index == 0 do
+          acc + n
+        else
+          acc + n * (1 <<< index * 4)
+        end
+      end)
+    else
+      0
     end
   end
 end
