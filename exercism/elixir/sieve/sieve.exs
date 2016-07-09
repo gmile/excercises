9e96147edd69fe060c0a6df33510a1f78ced4d19
Eugene Pirogov <iamexile@gmail.com>
2016-07-09 17:54:29 +0300

    [exorcism.io] [elixir] sieve (proper solution)

--- exercism/elixir/sieve/sieve.exs
+++ exercism/elixir/sieve/sieve.exs
@@ -5,9 +5,14 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
-    Enum.reduce(2..limit, [], fn(e, acc) ->
-      if Enum.all?(acc, &rem(e, &1) > 0), do: [e | acc], else: acc
-    end)
+    2
+    |> do_sieve(Enum.into(2..limit, []), [])
     |> Enum.reverse
+  end
+
+  defp do_sieve(n, [], acc), do: [n | acc]
+  defp do_sieve(n, list, acc) do
+    [new_n | survivors] = Enum.filter(list, fn(e) -> rem(e, n) != 0 end)
+    do_sieve(new_n, survivors, [n | acc])
   end
 end
