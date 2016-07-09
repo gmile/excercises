52fd27b921913d3f88cbb4771c721a9315cd2273
Eugene Pirogov <iamexile@gmail.com>
2016-07-09 17:31:35 +0300

    [exorcism.io] [elixir] hamming (update test suite)

--- exercism/elixir/hamming/hamming_test.exs
+++ exercism/elixir/hamming/hamming_test.exs
@@ -8,29 +8,28 @@ ExUnit.configure exclude: :pending, trace: true
 defmodule DNATest do
   use ExUnit.Case
 
-  # @tag :pending
   test "no difference between empty strands" do
-    assert DNA.hamming_distance('', '') == 0
+    assert DNA.hamming_distance('', '') == {:ok, 0}
   end
 
   test "no difference between identical strands" do
-    assert DNA.hamming_distance('GGACTGA', 'GGACTGA') == 0
+    assert DNA.hamming_distance('GGACTGA', 'GGACTGA') == {:ok, 0}
   end
 
   test "small hamming distance in middle somewhere" do
-    assert DNA.hamming_distance('GGACG', 'GGTCG') == 1
+    assert DNA.hamming_distance('GGACG', 'GGTCG') == {:ok, 1}
   end
 
   test "distance with same nucleotides in different locations" do
-    assert DNA.hamming_distance('TAG', 'GAT') == 2
+    assert DNA.hamming_distance('TAG', 'GAT') == {:ok, 2}
   end
 
   test "larger distance" do
-    assert DNA.hamming_distance('ACCAGGG', 'ACTATGG') == 2
+    assert DNA.hamming_distance('ACCAGGG', 'ACTATGG') == {:ok, 2}
   end
 
   test "hamming distance is undefined for strands of different lengths" do
-    assert DNA.hamming_distance('AAAC', 'TAGGGGAGGCTAGCGGTAGGAC') == nil
-    assert DNA.hamming_distance('GACTACGGACAGGACACC', 'GACATCGC') == nil
+    assert {:error, "Lists must be the same length."} = DNA.hamming_distance('AAAC', 'TAGGGGAGGCTAGCGGTAGGAC')
+    assert {:error, "Lists must be the same length."} = DNA.hamming_distance('GACTACGGACAGGACACC', 'GACATCGC')
   end
 end
