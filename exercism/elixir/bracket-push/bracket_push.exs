de3a00e448e5122604cfaa0c128b660f0bde583f
Eugene Pirogov <iamexile@gmail.com>
2016-07-11 19:38:35 +0300

    [exorcism.io] [elixir] bracket-push (fix)

--- exercism/elixir/bracket-push/bracket_push.exs
+++ exercism/elixir/bracket-push/bracket_push.exs
@@ -12,12 +12,9 @@ defmodule BracketPush do
   defp check(_, stack \\ [])
   defp check([], []), do: true
   defp check([], _), do: false
-  defp check([h1|t1], []), do: check(t1, [h1])
-
   defp check([h1|t1], stack) when h1 in '[{(' do
     check(t1, [h1 | stack])
   end
-
   defp check([h1|t1], [h2|t2]) when h1 in ']})' do
     if friends?(h2, h1) do
       check(t1, t2)
@@ -25,7 +22,6 @@ defmodule BracketPush do
       false
     end
   end
-
   defp check([_|t1], stack), do: check(t1, stack)
 
   defp friends?(?(, ?)), do: true
