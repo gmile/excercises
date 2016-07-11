cf39981d3f9e56e7d1e71f3b1fe3b2ec2a7b3f82
Eugene Pirogov <iamexile@gmail.com>
2016-07-11 21:47:34 +0300

    [exorcism.io] [elixir] bracket-push (broken)

--- exercism/elixir/bracket-push/bracket_push.exs
+++ exercism/elixir/bracket-push/bracket_push.exs
@@ -4,17 +4,14 @@ defmodule BracketPush do
   """
   @spec check_brackets(String.t) :: boolean
   def check_brackets(str) do
-    str
-    |> String.to_char_list
-    |> check
+    check(str, [])
   end
 
-  defp check(_, stack \\ [])
-  defp check([], []), do: true
-  defp check([], _), do: false
-  defp check([h1|t1], stack) when h1 in '[{(', do: check(t1, [h1 | stack])
-  defp check([h1|t1], [h2|t2]) when h1 in ']})', do: if pair?(h2, h1), do: check(t1, t2), else: false
-  defp check([_|t1], stack), do: check(t1, stack)
+  defp check(<<>>, <<>>), do: true
+  defp check(<<>>, _), do: false
+  defp check(<< h1, t1 :: binary >>, stack) when h1 in '[{(', do: check(t1, << h1 >> <> stack)
+  defp check(<< h1, t1 :: binary >>, << h2, t2 :: binary >>) when h1 in ']})', do: if pair?(h2, h1), do: check(t1, t2), else: false
+  defp check(<< _, t1 :: binary>>, stack), do: check(t1, stack)
 
   defp pair?(?(, ?)), do: true
   defp pair?(?[, ?]), do: true
