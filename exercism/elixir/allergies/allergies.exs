bb8f50380b3609dc0d2385458dc4d0238b5f7b8e
Eugene Pirogov <iamexile@gmail.com>
2016-07-13 13:55:02 +0300

    [exorcism.io] [elixir] allergies

---
+++ exercism/elixir/allergies/allergies.exs
@@ -0,0 +1,32 @@
+defmodule Allergies do
+  import Bitwise, only: ["|||": 2]
+
+  @allergies %{
+    1   => "eggs",
+    2   => "peanuts",
+    4   => "shellfish",
+    8   => "strawberries",
+    16  => "tomatoes",
+    32  => "chocolate",
+    64  => "pollen",
+    128 => "cats"
+  }
+
+  @doc """
+  List the allergies for which the corresponding flag bit is true.
+  """
+  @spec list(non_neg_integer) :: [String.t]
+  def list(flags) do
+    @allergies
+    |> Enum.filter(fn { value, _ } -> flags == (flags ||| value) end)
+    |> Enum.map(fn { _, allergy } -> allergy end)
+  end
+
+  @doc """
+  Returns whether the corresponding flag bit in 'flags' is set for the item.
+  """
+  @spec allergic_to?(non_neg_integer, String.t) :: boolean
+  def allergic_to?(flags, item) do
+    item in list(flags)
+  end
+end
