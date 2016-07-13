86109c5421e5319108f16d449a3a0a5baefd9f46
Eugene Pirogov <iamexile@gmail.com>
2016-07-13 15:11:31 +0300

    [exorcism.io] [elixir] wordy (1 test is grey)

---
+++ exercism/elixir/wordy/wordy.exs
@@ -0,0 +1,39 @@
+defmodule Wordy do
+  @doc """
+  Calculate the math problem in the sentence.
+  """
+  @spec answer(String.t) :: integer
+  def answer(<<"What is ", expression::binary>>) do
+    [ n | rest ] =
+      expression
+      |> String.replace("?", "")
+      |> String.replace("minus", "-")
+      |> String.replace("plus", "+")
+      |> String.replace("multiplied by", "*")
+      |> String.replace("divided by", "/")
+      |> String.split(" ")
+      |> Enum.map(fn(e) ->
+        case e |> Integer.parse do
+          { n, "" } -> n
+          :error -> e
+        end
+      end)
+
+    do_answer(rest, n)
+  end
+
+  def do_answer(list, acc \\ 0)
+  def do_answer([], acc), do: acc
+  def do_answer([op, n | rest], acc) do
+    acc = case op do
+      "+" -> acc + n
+      "-" -> acc - n
+      "*" -> acc * n
+      "/" -> acc / n
+    end
+
+    do_answer(rest, acc)
+  end
+
+  def answer(_), do: raise ArgumentError
+end
