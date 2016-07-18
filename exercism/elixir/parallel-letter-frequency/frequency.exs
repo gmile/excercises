91017c7f75c424018d10bae4ed813c0ad7445446
Eugene Pirogov <iamexile@gmail.com>
2016-07-18 14:47:54 +0300

    [exorcism.io] [elixir] frequency (done)

--- exercism/elixir/parallel-letter-frequency/frequency.exs
+++ exercism/elixir/parallel-letter-frequency/frequency.exs
@@ -10,21 +10,14 @@ defmodule Frequency do
   def frequency(texts, workers) do
     current = self()
 
-    IO.puts("#{inspect current}: spawning workers...")
-    pids =
-      Enum.map(1..workers, fn(_worker) ->
-        spawn(__MODULE__, :do_work_for, [current])
-      end)
-
-    IO.puts("#{inspect current}: sending jobs...")
-    pids
+    1..workers
+    |> Enum.map(fn(_worker) -> spawn(__MODULE__, :do_work_for, [current]) end)
     |> Stream.cycle()
     |> Enum.zip(texts)
     |> Enum.each(fn { worker_pid, text } ->
          send(worker_pid, { :work_on, text })
        end)
 
-    IO.puts("#{inspect current}: waiting for results from workers...")
     wait_for_all(length(texts), %{})
   end
 
@@ -32,25 +25,23 @@ defmodule Frequency do
   defp wait_for_all(jobs_left, final_result) do
     result =
       receive do
-        { :job_is_done, result } -> result
+        { :job_is_done, result } ->
+          Map.merge(final_result, result, fn(_, v1, v2) -> v1 + v2 end)
       end
 
-    wait_for_all(jobs_left - 1, Map.merge(final_result, result, fn(_, v1, v2) -> v1 + v2 end))
+    wait_for_all(jobs_left - 1, result)
   end
 
   def do_work_for(parent_pid) do
-    IO.puts("#{inspect self()}: waiting for work to do...")
     receive do
       { :work_on, text } ->
         result =
-          text
-          |> String.downcase()
-          |> String.graphemes()
+          Regex.scan(~r|\p{L}|u, String.downcase(text))
+          |> List.flatten()
           |> Enum.reduce(%{}, fn(c, acc) ->
             Map.update(acc, c, 1, &(&1 + 1))
           end)
 
-        IO.puts("#{inspect self()}: done working! Sending result to #{inspect parent_pid}...")
         send(parent_pid, { :job_is_done, result })
     end
 
